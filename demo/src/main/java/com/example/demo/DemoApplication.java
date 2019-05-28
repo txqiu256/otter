44de08897730828bffbeab413457021597687796
txqiu256 <txqiu256@126.com>
2019-05-28 14:25:03 +0800

    测试注解写入提交

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 加入注释,上传测试
+ */
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
 public class DemoApplication {
